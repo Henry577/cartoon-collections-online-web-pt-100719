1f7ae96f790e438beac3952cc2f1c7afac9aea59
Henry Allan <henryallan50@gmail.com>
2019-11-06 19:04:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(characters)
+  
+  characters.each do |name|
+    
+    puts name
+    
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
